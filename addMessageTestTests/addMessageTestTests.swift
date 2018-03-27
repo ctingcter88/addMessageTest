8235c9c341586f3c9e8ce8f01a4e5da4ea2eab4c
Gareth Miller <gmiller290488@gmail.com>
2018-03-27 08:29:21 +0100

    Initial Commit

---
+++ addMessageTestTests/addMessageTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  addMessageTestTests.swift
+//  addMessageTestTests
+//
+//  Created by Gareth Miller on 27/03/2018.
+//  Copyright © 2018 Gareth Miller. All rights reserved.
+//
+
+import XCTest
+@testable import addMessageTest
+
+class addMessageTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
